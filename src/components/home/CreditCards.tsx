17eb1c4dfc0fcc6dfc7ba8a2bbe749d0cfe75a2a
kazi331 <kazisharifulislam52@gmail.com>
2025-09-02 02:33:35 +0600

    px replaced with %

--- src/components/home/CreditCards.tsx
+++ src/components/home/CreditCards.tsx
@@ -3,7 +3,7 @@ import { cards, type Card } from "@/lib/dummy";
 export default function CreditCards() {
   return (
     <div className="flex justify-center h-[38rem] -mb-64 ">
-      {cards.map((card: Card, i: number) => (<CreditCard card={card} i={i} />))}
+      {cards.map((card: Card, i: number) => (<CreditCard card={card} i={i} key={card.id} />))}
     </div>
   );
 }
@@ -15,7 +15,7 @@ export const CreditCard = ({ card, i }: { card: Card, i: number }) => {
       <div
         style={{
           transform: `rotate(${(i + 1) * 30}deg)`,
-          transformOrigin: "450px 100px",
+          transformOrigin: "150% 50%",
         }}
         className={`w-max min-w-xs h-48 absolute bottom-0 right-0 -translate-x-1/2 rounded-3xl bg-gradient-to-br from-[#A68FC0]/80 via-[#8883B7]/80 to-[#A297C1]/80 backdrop-blur border-2 border-white/70 shadow font-manrope text-white `}
       >
